307059eb1417190b40ab47207f2d4076c9c0b1f0
David <fredidas@hotmail.com>
2022-07-04 15:15:10 +0200

    index

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-welcome to the chat !
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        hello
+        <?php
+            echo "hey im using php";
+        ?>
+    </h1>
+    <?php
+            echo "hey im using php";
+        ?>
+</body>
+</html>
