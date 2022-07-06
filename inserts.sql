b153c59f1fde9d4532d7da9ec4ec325a0c202d21
David <fredidas@hotmail.com>
2022-07-06 14:56:56 +0200

    inserts

--- inserts.sql
+++ inserts.sql
@@ -5,12 +5,10 @@ VALUES ('gevork@mail.com', 'goodpassword', 'hi im gevork', 1, 2); -- id 2
 INSERT INTO USERS (user_email,user_password,user_bio,user_theme,user_icon)
 VALUES ('hayk@mail.com', 'insanepassword', 'hi im hayk', 1, 4); -- id 3
 
-INSERT INTO GROUPCHAT (group_name,group_bio)
-VALUES ('Group A', 'the first group'); -- id 1
-INSERT INTO GROUPCHAT (group_name,group_bio)
-VALUES ('Group B', 'the second group'); -- id 2
-INSERT INTO GROUPCHAT (group_name,group_bio)
-VALUES ('Group C', 'the third group'); -- id 3
+INSERT INTO GROUPCHAT (group_name,group_bio, group_admin_id) VALUES 
+('Group A', 'the first group', 1), -- id 1
+('Group B', 'the second group', 1), -- id 2
+('Group C', 'the third group', 1); -- id 3
 
 INSERT INTO isInGroup(isInGroup_user_id, isInGroup_group_id) VALUES 
 (1, 1),
