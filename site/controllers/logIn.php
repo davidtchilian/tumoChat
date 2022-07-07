c30f1c12edd50f81d7f45b0657f97cb39d2098ce
GeorgeTumo <georgi.zargaryan.y@tumo.org>
2022-07-07 17:53:20 +0400

    LogIn

--- site/controllers/logIn.php
+++ site/controllers/logIn.php
@@ -1,19 +1,16 @@
 <?php 
 require_once('../models/db.php');
-session_start();
+
 
 $user_email = $_POST['user_email'];
 $user_pwrd = md5($_POST['user_pass']);
 
-$regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/'; 
-if (!preg_match($regex, $user_email)) {
-  header("Location: ../views/logIn.php?err=1");
-}
 $sql = "SELECT user_id FROM USERS WHERE user_email = '$user_email' AND user_password = '$user_pwrd'";
 $result = $conn->query($sql);
 
 if($result->num_rows == 1) {
     while($row = $result->fetch_assoc()) {
+        session_start();
         $_SESSION['user_id'] = $row["user_id"];
         header("Location: ../views/page-accueil.php");
     }
