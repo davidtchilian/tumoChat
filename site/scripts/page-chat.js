20ab9113caf5614cfc991d99a7addde109bbf880
MherZaqaryan <mher.zaqaryann@gmail.com>
2022-07-07 19:00:41 +0400

    add modal information menu about the group

---
+++ site/scripts/page-chat.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById("infoModal");
+var btn = document.getElementById("infoButton");
+var span = document.getElementById("closeButton");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
