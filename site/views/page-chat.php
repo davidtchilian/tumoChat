dcceb7b9fc136ed118d09806333d595d9eefc6b7
MherZaqaryan <mher.zaqaryann@gmail.com>
2022-07-07 16:06:13 +0400

    add echo for message content

--- site/views/page-chat.php
+++ site/views/page-chat.php
@@ -116,7 +116,7 @@
               <div class="row">
                 <div class="col-8">
                   <button type="button" class="btn btn-primary messageRecu mt-2" style="float : left; color: black;">
-                    <?php  $message['message_content'] ?>
+                    <?php echo $message['message_content'] ?>
                   </button>
                 </div>
                 <div class="col-4"></div>
