22dbf6d68dc1a3673fa3e59305164e28f355543d
arminehakobyan1 <armine.hakobyan1@tumo.org>
2022-07-08 15:58:22 +0400

    deleteuserfromgroup

--- site/controllers/deleteuserfromgroup.php
+++ site/controllers/deleteuserfromgroup.php
@@ -2,7 +2,7 @@
 require_once("../models/db.php");
 $groupid = $_GET['id'];
 $userid = $_SESSION['user_id'];
-$adminid = $_GET['group_admin_id'];
+$adminid = "SELECT group_admin_id FROM GROUPCHAT WHERE user_id = $userid";
 if($adminid == $userid){
     $result = mysqli_query($conn,$sql);
     $sql = "DELETE * FROM isINGROUP WHERE user_id = $userid";
