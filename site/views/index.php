c45ac875e64fdd292d6bf0e249062833b8b2212c
susanna1234567 <susanna.a.y@tumo.org>
2022-07-08 19:21:24 +0400

    index

--- site/views/index.php
+++ site/views/index.php
@@ -22,14 +22,10 @@
 </head>
 
 <body>
-    <div class="row">
+    <div class="flex-container">
         <div class="col-6">
             <img id="logo" src="../assets/images/fond_page_dacceuil.png" class="imaj" style="height: 1070px; width:auto">
         </div>
-        <div class="col-1"></div>
-        <div class="col-2">
-
-            
 
             <div class="main_buttons">
             <p></p>
@@ -49,10 +45,8 @@
                 <!-- Jump into the real world -->
                 Sign in
             </a>
-</div>
-        </div>
-        <div class="col-1"></div>
     </div>
+        </div>
     <script>
     </script>
 </body>
