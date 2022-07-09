480062e9bb9b041fb3d7899b23ffbd37ff6c3e53
Hakob-Harutyunyan <hakob.h8.y@tumo.org>
2022-07-09 07:56:28 +0400

    add group name on navbar

--- site/views/page-chat.php
+++ site/views/page-chat.php
@@ -44,9 +44,7 @@
             <div class="container">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#" style="color : white">
-                            <?php echo $groupName; ?>
-                        </a>
+                        <a class="nav-link" href="#" style="color : white"><?php echo $groupName; ?></a>
                     </li>
                 </ul>             
                 <div class="d-flex">
