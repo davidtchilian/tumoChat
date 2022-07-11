5f447679489cf4929e37a1040979202d16bb2266
Gevorg Khachatryan <gevorg.k9.y@tumo.org>
2022-07-11 19:31:16 +0400

    editmessage

--- site/views/page-chat.php
+++ site/views/page-chat.php
@@ -19,6 +19,12 @@
  
   $sql = "SELECT * FROM message WHERE message_group_id='$groupId'";
   $messages = mysqli_query($conn, $sql);
+  $message = mysqli_fetch_assoc($messages);
+
+
+  
+
+  
 
   $sql = "SELECT group_name FROM groupchat WHERE group_id='$groupId'";
   $groupName = mysqli_fetch_assoc(mysqli_query($conn, $sql))["group_name"];
@@ -36,8 +42,53 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/page-chat.css" />
     <title>Document</title>
+    <style>
+.dropbtn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+
+}
+
+.dropdown-content {
+  display: block;
+  position: relative;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+
+.dropdown:hover .dropbtn {
+  background-color: #3e8e41;
+}
+</style>
 </head>
 <body>
+    <script>
+
+
+    </script>
+  
+   
     <div class="fixed-top">
         <nav class="navbar navbar-expand-lg" style="background-color : #6c4b93">
             <a href="page-accueil.php"><img src="../assets/images/flèche_retour3.png" alt="Retour" style="width : 35px; height: 35px; margin-left: 10px" /></a>
@@ -54,7 +105,7 @@
                         </button>
                     </a>
                 </div>
-                <div id="infoModal" class="modal">
+                <div id="infoModal" class="modal" >
                     <div class="modal-content">
                         <p id="groupInfo"></p>
                         <ol id="usersInfo"></ol>
@@ -66,25 +117,35 @@
             </div>
         </nav>
     </div>
-    <div class="container mt-5" style="min-height : 100vh" style="position : relative">
+    <div class="container mt-5" style="min-height : 100vh"  style="position : relative" >
         <br>
         <?php
     while ($message = mysqli_fetch_assoc($messages)) {
         if ($message['message_sender_id'] == $userId) {
         ?>
-        <div class="row">
+        <div class="row" id = "messages" >
             <div class="col-4"></div>
             <div class="col-8">
-                <button class="btn btn-primary messageRecu mt-2" style="float : right; color: black;">
+                <button class="btn btn-primary messageEnvoye mt-2" style="float : right; color: black;" onclick="myFunction(event)" name="<?= $message['message_id']?> " id = "name">
                     <?php echo $message['message_content']; ?>
                 </button>
+                <div class="dropdown" style="width:30px; margin-left:900px; margin-top:-30px;">
+                
+                <div class="dropdown-content" id = "dropdown-content">
+                    <a href="#" onclick = "update()" id = "editId">Edit</a>
+                    <a href="#">Delete</a>
+                    <a href="#">Reply</a>
+                    </div>
+
+                </div> 
+    </div>
             </div>
-        </div>
+           
         <?php }
         else { ?>
         <div class="row">
             <div class="col-8">
-                <button type="button" class="btn btn-primary messageRecu mt-2" style="float : left; color: black;">
+                <button type="button" class="btn btn-primary messageRecu mt-2" style="float : left; color: black;" >
                     <?php echo $message['message_content'] ?>
                 </button>
             </div>
@@ -101,11 +162,13 @@
         <nav class="navbar navbar-expand-lg" style="background-color:#6c4b93">
             <div class="container">
                 <div class="container-fluid">
-                    <form class="d-flex" role="search" action="../controllers/sendmessage.php" method="post">
+                    <form class="d-flex" role="search" action="../controllers/sendmessage.php" method="post" id = "form">
                         <input type="hidden" name="user_id" value="<?php echo $userId; ?>">
                         <input type="hidden" name="group_id" value="<?php echo $groupId; ?>">
-                        <input name="message_content" class="form-control me-2" type="text" placeholder="Enter your message here" />
+
+                        <input name="message_content" class="form-control me-2" type="text" placeholder="Enter your message here" id = "text" value="gggg">
                         <button class="btn search" type="submit" value="Message">
+                            <a href="page-chat.php?id=<?php echo $groupId;?>"></a>
                             <img src="../assets/images/avion_papier_nour_1.png" alt="envoye" style="width :40px" style="height : 40px" />
                         </button>
                     </form>
@@ -113,7 +176,9 @@
             </div>
         </nav>
     </div>
-    <script src="../scripts/page-chat.js"></script>
+    <script src="../scripts/page-chat.js"> 
+
+   </script>
 
 </body>
 </html>
