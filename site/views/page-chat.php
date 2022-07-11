731ac35ff8557a326020988097803014436a01a7
Aram696 <aram.afunts.y@tumo.org>
2022-07-11 16:56:32 +0400

    chat name

--- site/views/page-chat.php
+++ site/views/page-chat.php
@@ -45,7 +45,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="../style/page-chat.css" />
-    <title>Document</title>
+    <title><?php echo $group_name." - TUYU"; ?></title>
 </head>
 <body>
     <div class="fixed-top">
