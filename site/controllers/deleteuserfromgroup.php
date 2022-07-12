9fc0efdf213022f1b187d798fe2b23ef896fa0cb
Hakob-Harutyunyan <hakob.h8.y@tumo.org>
2022-07-12 18:04:11 +0400

    delate user

--- site/controllers/deleteuserfromgroup.php
+++ site/controllers/deleteuserfromgroup.php
@@ -3,8 +3,9 @@ session_start();
 require_once("../models/db.php");
 $groupid = $_GET['id'];
 $deletingid = $_GET['delid'];
-$sql = "DELETE FROM isINGROUP WHERE user_id = $deletingid";
+$sql = "DELETE FROM isINGROUP WHERE isingroup_user_id = $deletingid AND isInGroup_group_id = $groupid";
 $result = mysqli_query($conn,$sql);
 mysqli_close();
+header("Location: ../views/page-chat.php?id=".$groupid."&modal=1");
 
 ?>
