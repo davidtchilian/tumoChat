52db31fe27e1747fc30b37ce49966ec7c4898520
VardanHeroic <vardan.tarayan@tumo.org>
2022-07-12 18:58:57 +0400

    search

--- site/scripts/search.js
+++ site/scripts/search.js
@@ -2,24 +2,22 @@ var button = document.querySelector('.search');
 var input = document.querySelector('.srch-input');
 var groups = document.getElementsByClassName('card');
 var names = document.getElementsByClassName('group-name');
+var namesArr = [];
 
+for (i = 0; i < groups.length; i++) {
+    namesArr[i] = names[i].innerHTML;
+}
 
-
-// button.onclick = function(){
-//     for (i = 0; i < groups.length; i++) {
-//         var grp_name = names[i].innerHTML;
-//         if (input.value != grp_name) {
-//             groups[i].setAttribute("style", "display: none;");
-//         };
-//     };
-//     return false;
-// };
-var anun;
-button.onclick = function clicked(){
-    anun = names.filter(function (element) {
-        return element.includes(input.value);
+button.onclick = function(){
+    var anun = namesArr.filter(function (elements) {
+        return elements.includes(input.value);
     })
-    console.log(anun);
+    // for (i = 0; i < groups.length; i++) {
+    //     groups[i].setAttribute("style", "display: none;");
+    //     if (anun.includes(String(names[i]))) {
+    //         groups[i].setAttribute("style", "display: block;");
+    //     };
+    // };
     return false;
 };
 
