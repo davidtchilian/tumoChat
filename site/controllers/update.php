d872b5aadaf5fd587a7ed153196e3077df9781a8
Hakob-Harutyunyan <hakob.h8.y@tumo.org>
2022-07-12 19:24:53 +0400

    edit.php

--- site/controllers/update.php
+++ site/controllers/update.php
@@ -2,5 +2,5 @@
   $user_id = $_POST["user_id"];
   $groupId = $_POST['group_id'];
   $message_conntent = $_POST["message_content"];
-  header("Location: ../views/page-chat.php?id=".$groupId);
+  header('Location: ../views/page-chat.php?id='.$groupId);
 ?>
