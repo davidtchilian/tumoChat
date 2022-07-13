c7e13b69074fd5016979cfe065b9ff8fd0637a15
Gevorg Khachatryan <gevorg.k9.y@tumo.org>
2022-07-13 18:42:23 +0400

    message edit

--- site/controllers/update.php
+++ site/controllers/update.php
@@ -10,21 +10,19 @@
 
 
   $sql2 = "SELECT `message_sender_id` FROM `MESSAGE` WHERE `message_id` = $message_id";
-  $result = mysqli_fetch_assoc(mysqli_query($conn, $sql2));
-  $message_sender_id = $result["message_sender_id"];
+  $result = mysqli_query($conn, $sql2);
+  $row = mysqli_fetch_assoc($result);
+  $message_sender_id = $row["message_sender_id"];
+  
+if(mysqli_num_rows($result) == 1 && $message_sender_id == $userId){
 
-
-
- if($message_sender_id == $userId){
     $sql = "UPDATE `MESSAGE` SET `message_content`='$message_conntent' WHERE `message_id` = $message_id AND `message_group_id` = $groupId";
     mysqli_query($conn, $sql);
     mysqli_close($conn);
+    header('Location: ../views/page-chat.php?id='.$groupId); exit();   
 
-  
-    header('Location: ../views/page-chat.php?id='.$groupId);
-  }else{
-    header('Location: ../views/page-chat.php?id='.$groupId);
-  }
-
+}else{
+  header('Location: ../views/page-chat.php?id='.$groupId); exit();
  
+}
 ?>
