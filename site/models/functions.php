038b4a483b5ff7817a617867551fc5d94277fde4
GeorgeTumo <georgi.zargaryan.y@tumo.org>
2022-07-25 18:20:16 +0400

    working add friends to group without styles

--- site/models/functions.php
+++ site/models/functions.php
@@ -45,7 +45,6 @@
         return $result -> num_rows > 0;
     }
 
-<<<<<<< HEAD
     function getgroupinfo($conn, $groupId){
         $sql = "SELECT * FROM GROUPCHAT WHERE group_id = $groupId";
         $result = mysqli_query($conn, $sql);
@@ -56,7 +55,6 @@
 
     }
 
-=======
     function getGroupUsersId($conn,$groupId){
         $users = array();
         $sql = "SELECT DISTINCT user_id
@@ -68,5 +66,4 @@
         }
         return $users;
     }
->>>>>>> ae62f0ac95ecfe89f8813aaa3f685244c9da6e0c
 ?>
