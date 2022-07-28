0e1d2907050478e010281caf4b8bc1b262911caa
MherZaqaryan <mher.zaqaryann@gmail.com>
2022-07-29 02:04:20 +0400

    some improvements

---
+++ site/config/config.php
@@ -0,0 +1,20 @@
+<!-- This is the place where you have to configure everything to start using TUMO Chat. -->
+
+<?php
+
+$config = array(
+
+    // MySQL Database Configuration
+    'DB_HOST' => 'localhost',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'root',
+    'DB_DATABASE' => 'tumochat',
+    'DB_PORT' => 3306,
+
+    // Domain Name
+    'DOMAIN_NAME' => 'http://localhost:8888/site',
+
+
+);
+
+?>
